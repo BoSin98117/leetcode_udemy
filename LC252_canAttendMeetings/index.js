8e96caecd22bae5d0328ae49b1a017e6e8fb5f6a
bo98117 <bosin98117@gmail.com>
2022-11-09 05:18:44 -0800

    LC252_canAttendMeetings

--- LC252_canAttendMeetings/index.js
+++ LC252_canAttendMeetings/index.js
@@ -4,6 +4,31 @@
 // canAttendMeetings([[0, 30], [5, 10], [15, 20]]) --> false
 // canAttendMeetings([[7, 10], [2, 4]]) --> true
 
-const canAttendMeetings = intervals => {};
+const canAttendMeetings = intervals => {
+    const starts = [];
+    const ends = [];
+
+    // Fill up STARTS and ENDS
+    for (let i = 0; i < intervals.length; i++) {
+        const subArray = intervals[i];
+        starts.push(subArray[0]);
+        ends.push(subArray[1]);
+    }
+    // Sort STARTS and ENDS array from least to greatest.
+    starts.sort((a, b) => a - b);
+    ends.sort((a, b) => a - b);
+
+    for (let i = 0; i < starts.length; i++) {
+        if (starts[i + 1] < ends[i]) return false;
+    }
+
+    return true;
+};
 
 module.exports = canAttendMeetings;
+
+
+/*
+Time Complexity: O(n log n) - We use a SORTING ALGORITHM for STARTS and ENDS array (we do  have a FOR LOOP which is O(n) but we simplify this to O(n log n) due to sorting algorithm).
+Space Complexity: O(n) - STARTS and ENDS array.
+*/
