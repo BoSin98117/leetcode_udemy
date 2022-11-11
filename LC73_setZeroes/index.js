7f553926418fc666e3ef8a7be0ff8bb4a4cc72ed
bo98117 <bosin98117@gmail.com>
2022-11-11 07:47:16 -0800

    LC73_setZeroes

--- LC73_setZeroes/index.js
+++ LC73_setZeroes/index.js
@@ -1,3 +1,86 @@
-function setZeroes(matrix) {}
+/*
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+You must do it in place.
+
+Example 1:
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+Example 2:
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]] 
+
+Constraints:
+m == matrix.length
+n == matrix[0].length
+1 <= m, n <= 200
+-231 <= matrix[i][j] <= 231 - 1 
+
+Follow up:
+A straightforward solution using O(mn) space is probably a bad idea.
+A simple improvement uses O(m + n) space, but still not the best solution.
+Could you devise a constant space solution?
+*/
+
+
+function setZeroes(matrix) {
+    let firstColHasZero = false;
+    let firstRowHasZero = false;
+
+    // Check if the First Column has zero
+    for (let i = 0; i < matrix.length; i++) {
+        if (matrix[i][0] === 0) {
+            firstColHasZero = true;
+            break;
+        }
+    }
+
+    // Check if the First Row has zero
+    for (let i = 0; i < matrix[0].length; i++) {
+        if (matrix[0][i] === 0) {
+            firstRowHasZero = true;
+            break;
+        }
+    }
+
+    // Use first row and column as flags, if rest of cells have zeros
+    for (let row = 1; row < matrix.length; row++) {
+        for (let col = 1; col < matrix[0].length; col++) {
+            if (matrix[row][col] === 0) {
+                matrix[0][col] = 0;
+                matrix[row][0] = 0;
+            }
+        }
+    }
+
+    // Zero out cells based on flags in first row and column
+    for (let row = 1; row < matrix.length; row++) {
+        for (let col = 1; col < matrix[0].length; col++) {
+            if (matrix[row][0] === 0 || matrix[0][col] === 0) {
+                matrix[row][col] = 0;
+            }
+        }
+    }
+
+    // Zero out first column
+    if (firstColHasZero) {
+        for (let i = 0; i < matrix.length; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+
+    // Zero out first row
+    if (firstRowHasZero) {
+        for (let i = 0; i < matrix[0].length; i++) {
+            matrix[0][i] = 0;
+        }
+    }
+}
 
 module.exports = setZeroes;
+
+
+/*
+Time Complexity: O(n * m) - Where N and M are length and width of Matrix.
+Space Complexity: O(1) - We modify the input Matrix in place.
+*/
