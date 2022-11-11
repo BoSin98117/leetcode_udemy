3837c15cdd1c5d2c0b57439edd82e3221de77901
bo98117 <bosin98117@gmail.com>
2022-11-11 09:43:57 -0800

    LC206_reverseList

--- LC206_reverseList/index.js
+++ LC206_reverseList/index.js
@@ -1,3 +1,43 @@
-const reverseList = head => {};
+/*
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
+
+Constraints:
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+*/
+
+
+const reverseList = head => {
+    let prev = null;
+    let current = head;
+
+    while (current) {
+        let temp = current.next;
+
+        current.next = prev;
+        prev = current;
+        current = temp;
+    }
+
+    return prev;
+};
 
 module.exports = reverseList;
+
+
+/*
+Time Complexity: O()
+Space Complexity: O()
+*/
