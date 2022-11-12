d5e59fae8aa52b5cf3399e6cc9f83142fcfb45dc
bo98117 <bosin98117@gmail.com>
2022-11-12 04:38:39 -0800

    LC19_removeNthFromEnd

--- LC19_removeNthFromEnd/index.js
+++ LC19_removeNthFromEnd/index.js
@@ -23,13 +23,31 @@ The number of nodes in the list is sz.
 
 
 function removeNthFromEnd(head, n) {
+    let dummyHead = { next: head };
+    let slow = dummyHead;
+    let fast = dummyHead;
 
+    // Move Fast N nodes ahead of Slow
+    for (let i = 1; i <= n; i++) {
+        fast = fast.next;
+    }
+
+    // Move Fast and Slow up one node at a time until Fast is at the last node
+    while (fast.next) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+    // After While Loop completes, Fast will be at the last node.
+    // Connect Slow to the node that is 2 nodes away, deleting the node Next to it.
+    slow.next = slow.next.next;
+
+    // Return the Head of the linked list with the node removed. 
+    return dummyHead.next;
 }
 
 module.exports = removeNthFromEnd;
 
-
 /*
-Time Complexity: O()
-Space Complexity: O()
+Time Complexity: O(n) - We traverse through the Linked List once.
+Space Complexity: O(1) - We always use 2 nodes, regardless of size of Linked List.
 */
