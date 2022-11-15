d15f0ac5b67e880a14ba916aa437f78fb01e2de7
bo98117 <bosin98117@gmail.com>
2022-11-15 14:05:04 -0800

    LC226_invertTree

--- LC226_invertTree/index.js
+++ LC226_invertTree/index.js
@@ -1,3 +1,45 @@
-function invertTree(root) {}
+/*
+Given the root of a binary tree, invert the tree, and return its root. 
+
+Example 1:
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+
+Example 2:
+Input: root = [2,1,3]
+Output: [2,3,1]
+
+Example 3:
+Input: root = []
+Output: [] 
+
+Constraints:
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+*/
+
+
+function invertTree(root) {
+    function helper(node) {
+        if (!node) return;
+
+        const temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+
+        helper(node.left);
+        helper(node.right);
+    }
+
+    helper(root);
+
+    return root;
+}
 
 module.exports = invertTree;
+
+
+/*
+Time Complexity: O(n) - We traverse through every Node in the Tree.
+Space Complexity: O(1)
+*/
