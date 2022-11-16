24cd8aa491a009e736cee4cb2ea7778c1c355855
bo98117 <bosin98117@gmail.com>
2022-11-16 12:02:56 -0800

    LC125_isPalindrome

--- LC125_isPalindrome/index.js
+++ LC125_isPalindrome/index.js
@@ -1,6 +1,34 @@
+/*
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise. 
+
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+Example 3:
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome. 
+
+Constraints:
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+*/
+
+
 function isPalindrome(s) {
     // Sanitize the input string (s)
-    // \W - grab all NON-Alphanumeric characters such as SPACES, COMMAS, including underscores (_)
+    // \W - grab all NON-Alphanumeric characters such as SPACES and COMMAS
+    // '_' - Include underscores(_) as well.
     // "" - means that we want to replace all NON-Alphanumeric characters with an empty string
     s = s.toLowerCase().replace(/[\W_]/g, "")
 
@@ -13,6 +41,7 @@ function isPalindrome(s) {
         if (s[left] !== s[right]) {
             return false;
         }
+        // Move Left pointer UP and Right pointer DOWN.
         left++;
         right--;
     }
