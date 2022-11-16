536b368399227fda636af7945028d2f421092594
bo98117 <bosin98117@gmail.com>
2022-11-16 10:13:27 -0800

    LC98_isValidBST

--- LC98_isValidBST/index.js
+++ LC98_isValidBST/index.js
@@ -1,3 +1,55 @@
-function isValidBST(root) {}
+/*
+Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+A valid BST is defined as follows:
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees. 
+
+Example 1:
+Input: root = [2,1,3]
+Output: true
+
+Example 2:
+Input: root = [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4. 
+
+Constraints:
+The number of nodes in the tree is in the range [1, 104].
+-231 <= Node.val <= 231 - 1
+*/
+
+
+function isValidBST(root) {
+    let isValid = true;
+
+    function helper(node, min, max) {
+        if (!node) return;
+
+        if ((min !== null && node.val <= min) ||
+            (max !== null && node.val >= max)) {
+            isValid = false;
+            return;
+        }
+
+        // As we traverse down to the LEFT, set MAX to the previous value (node.val)
+        // Every value has to be SMALLER than the previous.
+        helper(node.left, min, node.val);
+        // As we traverse down to the RIGHT, set MIN to the previous value (node.val)
+        // Every value has to be LARGER than the previous.
+        helper(node.right, node.val, max);
+    }
+
+    helper(root, null, null);
+
+    return isValid;
+}
 
 module.exports = isValidBST;
+
+
+/*
+Time Complexity: O(n) - We traverse through every Node in the Tree.
+Space Complexity: O(1)
+*/
