a016c12ef84d683c942b5f88580ee4a6f254cf94
bo98117 <bosin98117@gmail.com>
2022-11-18 04:51:10 -0800

    LC125_isPalindrome

--- LC125_isPalindrome/index.js
+++ LC125_isPalindrome/index.js
@@ -27,7 +27,7 @@ s consists only of printable ASCII characters.
 
 function isPalindrome(s) {
     // Sanitize the input string (s)
-    // \W - grab all NON-Alphanumeric characters such as SPACES and COMMAS
+    // \W - grab all NON-Alphanumeric characters such as SPACES, EXCLAMATION MARKS, etc..
     // '_' - Include underscores(_) as well.
     // "" - means that we want to replace all NON-Alphanumeric characters with an empty string
     s = s.toLowerCase().replace(/[\W_]/g, "")
@@ -53,4 +53,10 @@ module.exports = isPalindrome;
 /*
 Time Complexity - O(n) - Iterate through the string
 Space Complexity - O(1) - Left and Right pointers take up constant space
+*/
+
+/*
+"A man, a plan, a canal: Panama"
+After Sanitizing:
+amanaplanacanalpanama
 */
