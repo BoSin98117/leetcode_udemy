c9b915bcd6af1269a9ceb9b603065dd1e6b57437
bo98117 <bosin98117@gmail.com>
2022-11-24 08:52:58 -0800

    LC62_uniquePaths

--- LC62_uniquePaths/index.js
+++ LC62_uniquePaths/index.js
@@ -24,16 +24,27 @@ Constraints:
 
 
 function uniquePaths(m, n) {
-    const memo = Array(m).fill(Array(n).fill(1));
+    const dpMatrix = [];
 
-    for (let row = 1; row < m; row++) {
-        for (let col = 1; col < n; col++) {
-            // Fill up the 2d array with the values
-            memo[row][col] = memo[row - 1][col] + memo[row][col - 1];
+    for (let row = 1; row <= n; row++) {
+        dpMatrix.push([]);
+    }
+    // Fill out first row of dpMatrix
+    for (let row = 0; row < n; row++) {
+        dpMatrix[row][0] = 1;
+    }
+    // Fill out first col of dpMatrix
+    for (let col = 0; col < n; col++) {
+        dpMatrix[0][col] = 1;
+    }
+
+    for (let row = 1; row < n; row++) {
+        for (let col = 1; col < m; col++) {
+            dpMatrix[row][col] = dpMatrix[row][col - 1] + dpMatrix[row - 1][col];
         }
     }
-    // Return the value of the bottom right cell
-    return memo[m - 1][n - 1];
+
+    return dpMatrix[dpMatrix.length - 1][m - 1];
 }
 
 module.exports = uniquePaths;
@@ -47,4 +58,20 @@ Space Complexity: O(M * N) - memo array
 Get the value from the top dp[i -1][j]
 Get the value from the left dp[i][j - 1]
 dp[i][j] = dp[i -1][j] + dp[i][j - 1]
+*/
+
+
+
+/*
+*******************ALTERNATE SOLUTION******************************************
+    const memo = Array(m).fill(Array(n).fill(1));
+
+    for (let row = 1; row < m; row++) {
+        for (let col = 1; col < n; col++) {
+            // Fill up the 2d array with the values
+            memo[row][col] = memo[row - 1][col] + memo[row][col - 1];
+        }
+    }
+    // Return the value of the bottom right cell
+    return memo[m - 1][n - 1];
 */
