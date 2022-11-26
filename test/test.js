9b3b6b7334176c17c8a4730c3ae2478d4b6111a9
bo98117 <bosin98117@gmail.com>
2022-11-26 13:41:00 -0800

    LC152_maxProduct

--- test/test.js
+++ test/test.js
@@ -1,22 +1,8 @@
-function maxProduct(nums) {
-    let maxTillIndex = [nums[0]];
-    let minTillIndex = [nums[0]];
-    let max = nums[0];
-
-    for (let i = 1; i < nums.length; i++) {
-        const num = nums[i];
-
-        maxTillIndex[i] = Math.max(num,
-            num * maxTillIndex[i - 1],
-            num * minTillIndex[i - 1]);
-
-        minTillIndex[i] = Math.min(num,
-            num * maxTillIndex[i - 1],
-            num * minTillIndex[i - 1]);
-
-        max = Math.max(max, maxTillIndex[i]);
-    }
-    return max;
+function findMin(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+    const mid = Math.floor((left + right) / 2);
+    console.log(nums[mid]);
 }
 
-console.log(maxProduct([2, 8, -2, 4]));
+findMin([1, 2, 3, 4, 5, 6, 7, 8]);
