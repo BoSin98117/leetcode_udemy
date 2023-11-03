4911c708919987c6d68628cfc4c76f399a416801
bo98117 <bosin98117@gmail.com>
2023-11-03 08:52:57 -0700

    LC45_On

--- LC100_isSameTree/index.js
+++ LC100_isSameTree/index.js
@@ -17,7 +17,7 @@ Output: false
 Constraints:
 The number of nodes in both trees is in the range [0, 100].
 -104 <= Node.val <= 104
-*///
+*/////
 function isSameTree(p, q) {
     let sameTree = true;
 
